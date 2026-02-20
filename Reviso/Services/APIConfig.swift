b87a7d288065cb559b5754504edb397da939e570
vincent.woo <vincent.woo@okg.com>
2026-02-21 00:22:10 +0800

    feat: add APIConfig with hardcoded Poe API key

---
+++ Reviso/Services/APIConfig.swift
@@ -0,0 +1,13 @@
+//
+//  APIConfig.swift
+//  Reviso
+//
+//  Created by WOO Yu Kit Vincent on 21/2/2026.
+//
+
+import Foundation
+
+enum APIConfig {
+    /// Poe API key used for both answer erasing and question generation.
+    static let poeAPIKey = "6dkYqY8x7GH2JF_j9jHwO0sPL2nDgClL9V9CHxDJC4s"
+}
